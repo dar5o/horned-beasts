77be551d2266d08bc2458b798b444938438f0bcf
Dario Vitorte <vitortedario@gmail.com>
2021-09-13 14:50:33 -0700

    hotfix

--- src/components/hornedbeasts.js
+++ src/components/hornedbeasts.js
@@ -1,20 +1,15 @@
 import React from 'react';
-
-
 class HornedBeast extends React.Component {
   render() {
-
     return (
-
-        <section>
+      <section>
             <h2>{this.props.title}</h2>
-
             <img src={this.props.imageUrl} alt="horned" title="horned" />
 
             <p>{this.props.description}</p>
         </section>
-
     );
 }
 }
+
 export default HornedBeast;
