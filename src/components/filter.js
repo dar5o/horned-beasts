b5d5d4b5774c1bb7e4de1d6f586dee960e819a55
Dario Vitorte <vitortedario@gmail.com>
2021-09-16 14:43:05 -0700

    hotfix

--- src/components/filter.js
+++ src/components/filter.js
@@ -12,7 +12,7 @@ class Filter extends React.Component {
         <option value='1'>1 Horn</option>
         <option value='2'>2 Horns</option>
         <option value='3'>3 Horns</option>
-        <option value='100'>100 Horns</option>
+        <option value='100'>100+ Horns</option>
       </Form.Control>
       <br />
       <hr />
